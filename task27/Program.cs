6db60d357caceaf666930f53de020b32218fbb84
«SimLove» <simonolyuba@yandex.ru>
2023-06-01 21:42:55 +0300

    add task27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sum(int number)
+{
+    int resultSum=0;  
+    while(number>9)
+    {
+        resultSum+=(number%10);
+        number=number/10;
+    }
+    resultSum+=number;    
+    return resultSum;
+}
+
+Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма цифр числа: "+sum(num));
