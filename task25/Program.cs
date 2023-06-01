7caa0768f0735e433f366f735382cc2c67ce6cb7
«SimLove» <simonolyuba@yandex.ru>
2023-06-01 21:28:30 +0300

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+double pow(int number,int power)
+{
+    return Math.Pow(number,power);
+}
+
+Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую возводят число: ");
+int power=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Результат: "+pow(num,power));
