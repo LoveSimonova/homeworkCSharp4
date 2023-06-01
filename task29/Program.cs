6c15f0b7726aa250b2bf62029d6bd81d81e739f3
«SimLove» <simonolyuba@yandex.ru>
2023-06-01 22:20:10 +0300

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array(int length, int minPossibleNumber, int maxPossibleNumber)
+{
+    Random random = new Random();
+    int[] array = new int[length];
+    for(int i=0;i<length;i++)
+        array[i]=random.Next(minPossibleNumber,maxPossibleNumber);
+    return array;
+}
+
+void print(int[] array)
+{
+    for(int i=0;i<array.Length;i++)
+        Console.Write(""+array[i]+" ");
+}
+
+Console.WriteLine("Введите количество элементов массива: ");
+int len=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное число: ");
+int min=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число: ");
+int max=Convert.ToInt32(Console.ReadLine());
+int[] numbers=array(len,min,max);
+Console.Write("Массив: ");
+print(numbers);
